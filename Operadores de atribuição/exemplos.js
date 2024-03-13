59b36f9b99d0c7fdc1da9ab1d06ba60019de5ce9
elucasrg <lrggbi@gmail.com>
2024-03-13 16:55:17 -0300

    operadores de atribuição

---
+++ Operadores de atribuição/exemplos.js
@@ -0,0 +1,19 @@
+let foo = 2;
+const bar = 3;
+
+foo += bar;
+console.log(foo);
+
+foo *= bar;
+console.log(foo);
+
+/*let foo = 2;
+
+foo =+ '9'; // foo = +'9' = 9
+console.log(foo); // 9
+
+foo =- '9'; // foo = -'9' = -9
+console.log(foo); // -9
+
+foo =* 5; // Erro de sintaxe
+console.log(foo); // Não é executado pois ocorreu um erro na linha anterior*/
