af2b12111d85515b45af0dbedb7581a683200963
elucasrg <lrggbi@gmail.com>
2024-03-19 20:54:52 -0300

    questoes

--- Atividades/questão04.js
+++ Atividades/questão04.js
@@ -2,3 +2,4 @@ function receberNomeDoMes(numeroDoMes) {
     const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
     return meses[--numeroDoMes];
+}
---
+++ Atividades/questão05.js
@@ -0,0 +1,6 @@
+function igualOuMaior(n1, n2){
+    if(typeof n1 != typeof n2){
+        return false
+    }
+    return n1 >= n2;
+}
